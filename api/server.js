b3498c0d7f2a4ca6f3ebe9bb584e2cd247443bbe
unknown <michaeljogoh@gmail.com>
2024-07-01 11:30:57 +0100

    Update server.js to write file operations

--- api/server.js
+++ api/server.js
@@ -3,10 +3,18 @@ const jsonServer = require('json-server')
 
 const server = jsonServer.create()
 
+// Uncomment to allow write operations
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('task.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
+
 
 
 // Comment out to allow write operations
-const router = jsonServer.router('task.json')
+// const router = jsonServer.router('task.json')
 
 const middlewares = jsonServer.defaults()
 
